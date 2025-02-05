b3c7f240668bb972909cb0d89211491670174830
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2025-02-05 17:43:02 -0500

    Add stubs for Test implementation

--- Include/UART_RP2040.h
+++ Include/UART_RP2040.h
@@ -20,20 +20,20 @@
 /************************************************************
   INCLUDES
 ************************************************************/
-#if !defined( VIRTUAL_TARGET )
+
 #include "Reset_RP2040.h"
 #include "BRS_RP2040.h"
-#endif /* !VIRTUAL_TARGET */
-
 #include "ComStack_Types.h"
 #include "UART_RP2040_SFR.h"
 #include "UART_RP2040_cfg.h"
+
 /************************************************************
   ENUMS AND TYPEDEFS
 ************************************************************/
 #if defined( VIRTUAL_TARGET )
 volatile tRP2040_UART UART_Live;
 #endif /* !VIRTUAL_TARGET */
+
 /************************************************************
   EXTERN FUNCTIONS
 ************************************************************/
@@ -43,12 +43,38 @@ volatile tRP2040_UART UART_Live;
 ************************************************************/
 
 /**
- * @brief Adds all module init functions to the OS Init lists
+ * Initializes the UART according to the UART_RP2040_Config structure. Initializes the UART Baudrate and Control registers
+ *  Prepares the UART for Transmissiona AND reception. Assumes that the clock path is already prepared for the UART peripheral.
  * 
- * @note Should be the only place to call OS_ADDINIT()
+ * @param config: initial config for the UART peripheral, including baudrate and clock configurations.
+ * @return 
+ *         0: 'E_COM_OK' if the initialization is successful
+ *         1: 'E_COM_NOT_OK' if the initialization is not successful, typically due to misconfigured configuration struct
+ *
+ * @pre  Clockpaths to UART are already initialized.
+ * @post n/a
+ * @invariant n/a
+ *
  */
 extern Std_ComErrorCode UART_RP2040_InitSync( UART_RP2040_Config * config );
 
+/**
+ * Transmits data given to the UART peripheral hardware. Stores the data into the TxFifo. In the case that
+ *  Data can not fit in the TxFifo (i.e. because the TxFifos are not flushing in time), reports E_COM_BUSY.
+ * 
+ * @param databuffer: pointer to bytes to be transferred on the UART.
+ * @param length: number of bytes to transfer on UART from the buffer. UINT8 -> Maximum of 255 bytes to transfer.
+ * @return 
+ *         0: 'E_COM_OK' if the Transfer is successful
+ *         1: 'E_COM_NOT_OK' if the Transfer is not successful, typically due to unsupported input parameters
+ *         2: 'E_COM_BUSY' if the FIFO is full and it is not possible to add the data to the FIFO.
+ *      0xFF: 'E_COM_UNKNOWN' in the case of early return from subfunction. Default return value, but should be overwritten.
+ *
+ * @pre  UART is initialized.
+ * @post n/a
+ * @invariant n/a
+ *
+ */
 extern Std_ComErrorCode UART_RP2040_TransferSync ( uint8 * buffer, uint8 length);
 
 #endif /* UART_RP2040_H */
